bb52f1b5e36e806dde7b2e79eb409987a43c2655
xavier.niquet <niquet_xavier2002@yahoo.fr>
2020-05-10 11:21:41 +0200

    stage 3

--- hypercar/urls.py
+++ hypercar/urls.py
@@ -13,12 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
-from tickets.views import WelcomeView,menuView
+from django.urls import path,re_path
+from tickets.views import WelcomeView,menuView,TicketView
 
 
 urlpatterns = [
     path('welcome/', WelcomeView.as_view()),
-    path("menu/",menuView)
+    path("menu/",menuView),
+    re_path(r"^get_ticket/(?P<operation>\w+)$",TicketView.as_view())
 
 ]
--- tickets/views.py
+++ tickets/views.py
@@ -2,6 +2,52 @@ from django.views import View
 from django.http.response import HttpResponse
 from django.shortcuts import render
 
+from collections import deque,defaultdict
+from datetime import datetime,timedelta
+
+class TicketQueue:
+    queue=defaultdict(deque)
+    index=0
+    time={"change_oil":2 , "inflate_tires":5,"diagnostic":30}
+
+
+
+    def updateQueue(self,operation):
+        current_time=datetime.now()
+        queue=self.queue[operation]
+        delta=timedelta(minutes=self.time[operation])
+        if len(queue)>0:
+            while current_time - queue[0] > delta :
+                queue.popleft()
+
+    def update(self):
+        for operation in self.time.keys():
+            self.updateQueue(operation)
+
+
+
+    def get_waiting(self,operation):
+        waiting = lambda operation : len(self.queue[operation]) * self.time[operation]
+
+        if operation=="change_oil":
+            return waiting(operation)
+        elif operation=="inflate_tires":
+            return waiting("change_oil")  + waiting(operation)
+        else:
+            return waiting("change_oil") + waiting("inflate_tires")  + waiting(operation)
+
+    def add(self,operation):
+        self.update()
+        self.index+=1
+        time_wait=self.get_waiting(operation)
+        self.queue[operation].append(datetime.now())
+        return (time_wait,self.index)
+
+
+
+
+
+
 class WelcomeView(View):
     def get(self, request, *args, **kwargs):
         response="<h2>Welcome to the Hypercar Service!</h2>"
@@ -10,3 +56,13 @@ class WelcomeView(View):
 
 def menuView(request):
     return render(request,"tickets/menu.html")
+
+
+class TicketView(View):
+    ticketqueue=TicketQueue()
+    def get(self,request,*args,**kwargs):
+        time,index = self.ticketqueue.add(kwargs["operation"])
+
+        return render(request,"tickets/tickets.html",context={"index":index,"time":time})
+
+
